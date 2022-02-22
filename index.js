780fed97839cfdbedabc86568196b516bb6577d1
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-02-21 21:36:38 -0800

    added javascript code for buttons and rendering

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let tasks = []
+console.log(tasks)
 let total = 0
 let carWash = false
 let mowLawn = false
@@ -7,32 +8,75 @@ const carwashBtn = document.querySelector(".car-wash-btn")
 const mowLawnBtn = document.querySelector(".mow-lawn-btn")
 const pullWeedsBtn = document.querySelector(".pull-weeds-btn")
 
+const servicesSec = document.getElementById('services-sec')
+
+/// this is the div that will contain both p's ///
+// const taskDiv = document.createElement("div")
+// taskDiv.classList.add('task-div')
+
+// //// these are the paragraphs that will contain the task and price
+// const taskSet = document.create("p");
+// task.classList.add('task-set')
+// const taskPrice = document.create("p")
+// taskPrice.classList.add('price-set')
+
+
+
+
+function render(arr){
+    if(arr){
+        for(let i = 0; i < tasks.length; i++){
+            
+            tasks = []
+            
+            const taskDiv = document.createElement("div")
+            taskDiv.classList.add("task-div")
+            const taskSet = document.createElement("p");
+            taskSet.classList.add('task-set')
+            const taskPrice = document.createElement("p")
+            taskPrice.classList.add('price-set')
+            taskSet.textContent = arr[i]
+            taskDiv.append(taskSet)
+            
+            
+            taskDiv.append(taskPrice)
+            if(arr[i] === "car wash"){
+                taskPrice.textContent = 10
+                taskDiv.append(taskPrice)
+            } else if(arr[i] === "mow lawn"){
+                taskPrice.textContent = 20
+                taskDiv.append(taskPrice)
+            } else{
+                taskPrice.textContent = 30
+                taskDiv.append(taskPrice)
+            }
+            
+            servicesSec.append(taskDiv)
+        }
+    }
+}
 
 carwashBtn.addEventListener("click", function(){
     if(carWash === false){
         tasks.push("car wash")
-        total += 10;
         carWash = true;
-        console.log(tasks)
+        
+        render(tasks)
     }
 })
 
 mowLawnBtn.addEventListener("click", function(){
     if(mowLawn === false){
         tasks.push("mow lawn")
-        total += 20
-        mowLawn = true;
-        console.log(tasks)
+        mowLawn = true
+        render(tasks)
     }
 })
 
 pullWeedsBtn.addEventListener("click", function(){
     if(pullWeeds === false){
         tasks.push("pull weeds")
-        total += 30
         pullWeeds = true;
-        console.log(tasks)
+        render(tasks)
     }
 })
-
-/// Need to add code to display each array item to the dom as it's added to the array
