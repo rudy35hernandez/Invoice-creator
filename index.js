e4a3645a24fc8974ccb1de96925e5ead16806325
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-02-22 23:41:16 -0800

    added event listeners array and booleans

---
+++ index.js
@@ -0,0 +1,32 @@
+let tasks = []
+let total = 0
+let carWash = false
+let mowLawn = false
+let pullWeeds = false
+const carwashBtn = document.querySelector(".car-wash-btn")
+const mowLawnBtn = document.querySelector(".mow-lawn-btn")
+const pullWeedsBtn = document.querySelector(".pull-weeds-btn")
+
+carwashBtn.addEventListener("click", function(){
+    if(carWash === false){
+        tasks.push("car wash")
+        carWash = true;
+        console.log(tasks)
+    }
+})
+
+mowLawnBtn.addEventListener("click", function(){
+    if(mowLawn === false){
+        tasks.push("mow lawn")
+        mowLawn = true;
+        console.log(tasks)
+    }
+})
+
+pullWeedsBtn.addEventListener("click", function(){
+    if(pullWeeds === false){
+        tasks.push("pull weeds")
+        pullWeeds = true;
+        console.log(tasks)
+    }
+})
